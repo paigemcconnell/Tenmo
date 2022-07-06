2299cd8f5ee7baacc81fdc49bfbfb5cf151948be
Katie Kroell <kroell.k@gmail.com>
2022-07-06 11:20:18 -0400

    Pushing first hour

---
+++ TenmoServer/Controllers/TransferController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TenmoServer.Security;
+
+namespace TenmoServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TransferController : ControllerBase
+    {
+        public TransferController()
+        {
+
+        }
+
+        /*
+        [HttpGet]
+        [Authorize]
+        public ActionResult DisplayBalance()
+        {
+
+        }
+        */
+
+
+
+    }
+}
